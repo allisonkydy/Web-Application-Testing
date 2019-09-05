a3b983e961f0d15c23d4b19cc3bc4f865a41ea87
allisonkydy <allisnelly@gmail.com>
2019-09-05 16:07:39 -0500

    wrote tests for displaying dom elements

--- baseball-scoreboard/src/App.test.js
+++ baseball-scoreboard/src/App.test.js
@@ -2,6 +2,20 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-it('renders without crashing', () => {
-  
+test('renders without crashing', () => {
+  render(<App />);
 });
+
+test('balls and strikes are displaying', () => {
+  const { getByText } = render(<App />);
+  getByText(/balls/i);
+  getByText(/strikes/i);
+});
+
+test('dashboard buttons are displaying', () => {
+  const { getByTestId } = render(<App />);
+  getByTestId("strike-btn");
+  getByTestId("ball-btn");
+  getByTestId("foul-btn");
+  getByTestId("hit-btn");
+})
--- baseball-scoreboard/src/components/Dashboard.js
+++ baseball-scoreboard/src/components/Dashboard.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React from "react";
 
 const Dashboard = ({ handleStrike, handleBall, handleFoul, handleHit }) => {
   return (
     <div>
-      <button onClick={() => handleStrike()}>Strike</button>
-      <button onClick={() => handleBall()}>Ball</button>
-      <button onClick={() => handleFoul()}>Foul</button>
-      <button onClick={() => handleHit()}>Hit</button>
+      <button onClick={() => handleStrike()} data-testid="strike-btn">
+        Strike
+      </button>
+      <button onClick={() => handleBall()} data-testid="ball-btn">
+        Ball
+      </button>
+      <button onClick={() => handleFoul()} data-testid="foul-btn">
+        Foul
+      </button>
+      <button onClick={() => handleHit()} data-testid="hit-btn">
+        Hit
+      </button>
     </div>
-  )
-}
+  );
+};
 
 export default Dashboard;
